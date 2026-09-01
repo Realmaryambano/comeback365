8fe7f739bcc0da70effa85ad85270a994bcd5f22
Maryam Bano <maryambano.official@gmail.com>
2026-09-01 14:29:13 +0500

    Create index.py

---
+++ Day-001/index.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is:", sum)
+print("The product of", num1, "and", num2, "is", num1*num2,".")
+print(f"The reminder of {num1} and {num2} is {num1 / num2}.")
+print("The difference of", num1, "and", num2, "is", str(num1 - num2) + ".")
